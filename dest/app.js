39b60a29c604f3c8f18e8ee97d3abe9980be78ef
Judy Wambui <wambuijuddie065@gmail.com>
2022-08-14 13:40:11 +0300

    restructured code

--- dest/app.js
+++ dest/app.js
@@ -1,4 +1,3 @@
-import { render } from "./render.js";
 const taskInput = document.querySelector("#taskInput");
 const imageInput = document.querySelector("#imageInput");
 const dateInput = document.querySelector("#dateInput");
@@ -8,7 +7,10 @@ const closeIcon = document.querySelector("#closeIcon");
 const form = document.querySelector("#form");
 const phoneDisplay = document.querySelector(".phoneDisplay");
 const formDisplay = document.querySelector(".formDisplay");
-const streaksList = document.querySelector("#streaksList");
+let streaksList = document.querySelector("#streaksList");
+const activityDesc = document.querySelector("#activityDesc");
+const middle = document.querySelector("#middle");
+const aStreak = document.querySelector(".aStreak");
 plusIcon.addEventListener("click", () => {
     myFunctionPlus();
 });
@@ -23,21 +25,82 @@ function myFunctionClose() {
     phoneDisplay.style.display = "block";
     formDisplay.style.display = "none";
 }
-export class Streaks {
+class Streaks {
     constructor() {
         this.streaksArr = [];
+        this.displayStreak();
     }
-    getstreaksArr() {
+    getStreaks() {
         return this.streaksArr;
     }
-    addStreak(streakItem) {
-        this.streaksArr.push(streakItem);
+    addStreak(newStreak) {
+        this.streaksArr.push(newStreak);
+        this.displayStreak();
     }
-    getAllStreaks() {
-        return this.streaksArr;
+    deleteStreak(id) {
+        this.streaksArr.splice(id, 1);
+        this.displayStreak();
     }
-    getStreakById(id) {
-        return this.streaksArr.find((streak) => streak.id === id);
+    displayStreak() {
+        if (this.streaksArr.length === 0) {
+            activityDesc.textContent = "You Don't Have Any Activities!!!";
+        }
+        else {
+            activityDesc.textContent = "Activities";
+        }
+        streaksList.innerHTML = '';
+        this.streaksArr.map((streak, id) => {
+            let html = `
+                
+                    <div class="aStreak">
+                      <div class="streakImage">${streak.icon}</div>
+                      <div class="streakDate">${streak.date}</div>
+                      <div class="streakText">${streak.taskName}</div>
+                    </div>
+            
+                `;
+            streaksList.insertAdjacentHTML('beforeend', html);
+            for (let i = 0; i < streaksList.children.length; i++)
+                streaksList.children[i].addEventListener('click', (e) => {
+                    e.preventDefault();
+                    this.displayModal(i);
+                    const closeBtn = document.querySelector("#closeBtn");
+                    closeBtn.onclick = () => {
+                        middle.style.display = 'none';
+                    };
+                    const deleteBtn = document.querySelector("#deleteBtn");
+                    deleteBtn.onclick = () => {
+                        this.deleteStreak(id);
+                        middle.style.display = 'none';
+                    };
+                });
+        });
+    }
+    displayModal(index) {
+        const onePop = this.streaksArr[index];
+        let todaysDate = new Date();
+        let date = new Date(onePop.date);
+        let strkStart = todaysDate.getTime();
+        let beststreak = date.getTime();
+        let diff = Math.ceil((beststreak - strkStart) / (24 * 3600 * 1000));
+        middle.innerHTML = '';
+        let modal = `
+        <div class="popUp">
+        <div class="icon">${onePop.icon}</div>
+        <div class="date">${onePop.date}</div>
+        <div class="description">${onePop.taskName}</div>
+        <div class="days">${diff} days</div>
+        <div class="actions">
+          <div class="btn1">
+            <button type="button" id="closeBtn"> Close</button>
+          </div>
+          <div class="btn2">
+            <button type="button" id="deleteBtn"> Delete</button>
+          </div>
+        </div>
+      </div>
+        `;
+        middle.insertAdjacentHTML('beforeend', modal);
     }
 }
 const streak = new Streaks();
@@ -49,7 +112,16 @@ form.addEventListener("submit", (e) => {
         date: dateInput.value,
         icon: imageInput.value,
     };
-    streak.addStreak(streakItem);
-    form.reset();
-    streaksList.innerHTML = render(streak.getstreaksArr());
+    if (streakItem.date === '' || streakItem.taskName === '' || streakItem.icon === '') {
+        const topH = document.querySelector(".topH");
+        topH.style.display = 'block';
+        setTimeout(() => {
+            topH.style.display = "none";
+        }, 5000);
+    }
+    else {
+        streak.addStreak(streakItem);
+        form.reset();
+    }
 });
+export {};
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { streakInterface } from "./interfaces/streakInterface.js";
-import {render} from "./render.js";
+
 
 
 const taskInput = document.querySelector("#taskInput") as HTMLInputElement;
@@ -11,7 +11,10 @@ const closeIcon = document.querySelector("#closeIcon") as HTMLButtonElement;
 const form= document.querySelector("#form") as HTMLFormElement;
 const phoneDisplay= document.querySelector(".phoneDisplay") as HTMLElement;
 const formDisplay= document.querySelector(".formDisplay") as HTMLFormElement;
-const streaksList= document.querySelector("#streaksList") as HTMLUListElement;
+let streaksList= document.querySelector("#streaksList") as HTMLUListElement;
+const activityDesc = document.querySelector("#activityDesc") as HTMLDivElement;
+const middle = document.querySelector("#middle") as HTMLDivElement;
+const aStreak= document.querySelector(".aStreak") as HTMLDivElement
 
 
 plusIcon.addEventListener("click", () => {
@@ -30,50 +33,146 @@ function myFunctionClose() {
     formDisplay.style.display = "none";
  }
 
-
-export class Streaks{
+class Streaks{
     private streaksArr: streakInterface[] = [];
     constructor(){
+        this.displayStreak()
 
     }
-    getstreaksArr(){
+    getStreaks(){
         return this.streaksArr;
 
     }
-    addStreak(streakItem:streakInterface){
-        this.streaksArr.push(streakItem);
+    addStreak(newStreak:streakInterface){
+        this.streaksArr.push(newStreak);
+        this.displayStreak()
     }
-    getAllStreaks(){
-        return this.streaksArr;
+    deleteStreak(id:number){
+        this.streaksArr.splice(id,1)
+        this.displayStreak()
 
     }
-    getStreakById(id:number){
-        return this.streaksArr.find((streak) => streak.id === id);
+
+    displayStreak(){
+
+      
+            if(this.streaksArr.length===0){
+                activityDesc.textContent="You Don't Have Any Activities!!!"
+    
+            }else{
+                activityDesc.textContent="Activities"
+
+            }
+          
+            streaksList.innerHTML='';
+          
+                this.streaksArr.map((streak:streakInterface,id)=>{
+                let html=`
+                
+                    <div class="aStreak">
+                      <div class="streakImage">${streak.icon}</div>
+                      <div class="streakDate">${streak.date}</div>
+                      <div class="streakText">${streak.taskName}</div>
+                    </div>
+            
+                `
+                streaksList.insertAdjacentHTML('beforeend', html)
+
+                for (let i = 0; i < streaksList.children.length; i++)
+                streaksList.children[i].addEventListener('click', (e) =>{
+                    e.preventDefault()
+                    this.displayModal(i);
+                    const closeBtn = document.querySelector("#closeBtn") as HTMLButtonElement
+                    closeBtn.onclick = () => {
+                        middle.style.display = 'none';
+                    }
+                    const deleteBtn = document.querySelector("#deleteBtn") as HTMLButtonElement
+                    deleteBtn.onclick = () => {
+                        this.deleteStreak(id)
+                        middle.style.display = 'none';
+                    }
+                        
+                })
+
+            })
+               
+        
+    }
+    displayModal(index:number){
+        
+       
+        const onePop=this.streaksArr[index]
+        let todaysDate = new Date();
+        let date = new Date(onePop.date);
+        let strkStart = todaysDate.getTime();
+        let beststreak = date.getTime();
+        let diff = Math.ceil((beststreak - strkStart) / (24 * 3600 * 1000));
+        middle.innerHTML=''
+        let modal= `
+        <div class="popUp">
+        <div class="icon">${onePop.icon}</div>
+        <div class="date">${onePop.date}</div>
+        <div class="description">${onePop.taskName}</div>
+        <div class="days">${diff} days</div>
+        <div class="actions">
+          <div class="btn1">
+            <button type="button" id="closeBtn"> Close</button>
+          </div>
+          <div class="btn2">
+            <button type="button" id="deleteBtn"> Delete</button>
+          </div>
+        </div>
+      </div>
+        `
+        middle.insertAdjacentHTML('beforeend', modal)
 
     }
+   
+
+
 
 }
+   
+  
+
 const streak = new Streaks ();
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
     let streakItem: streakInterface = {
-        id:Math.ceil(Math.random() * 100),
+         id:Math.ceil(Math.random() * 100),
         taskName: taskInput.value,
         date: dateInput.value,
         icon: imageInput.value,
      
     };
 
+    if(streakItem.date===''||streakItem.taskName ===''||streakItem.icon === '') {
+        const topH= document.querySelector(".topH") as HTMLParagraphElement
+       
+        topH.style.display='block'
+        setTimeout(()=>{  
+            topH.style.display="none"
+         },5000)
+        
+        
+        
+    } else{
+        
+
+
 
     
 
     streak.addStreak(streakItem);
     form.reset();
-    streaksList.innerHTML = render(streak.getstreaksArr());
+    }
+    
    
   }
 );
+
+
 
 
 
